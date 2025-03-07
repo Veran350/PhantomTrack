4b8fcd3009354fef03a8c5a4b46ff9897c9b9402
VERAN-TECH-HUB <veranmaina685@gmail.com>
2025-03-07 01:43:32 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+pkg update -y && pkg install python sqlite -y  
+pip install phonenumbers pyfiglet requests  
+git clone https://github.com/Veran350/NaijaHijack  
+cd NaijaHijack  
+python naija.py  
